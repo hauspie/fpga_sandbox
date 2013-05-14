5485ca71ee4dca25a9a4af4dddc11ddfd11b84ce
Michaël Hauspie <michael.hauspie@lifl.fr>
2013-05-14 17:32:13 +0200

    Adds a second output to the clock divider

--- led_blink/clock_divider.vhd
+++ led_blink/clock_divider.vhd
@@ -31,25 +31,27 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity clock_divider is
     Port ( CK_IN : in  STD_LOGIC;
-           CK_OUT : out  STD_LOGIC);
+           CK_OUT : out  STD_LOGIC;
+           NCK_OUT : out STD_LOGIC);
 end clock_divider;
 
 architecture Behavioral of clock_divider is
 constant TIMECONST : integer := 100000000;
-signal count : integer range 0 to 100000000;
 signal D : std_logic := '0';
 begin
-process (CK_IN, D)
-begin
-	if (CK_IN'event and CK_IN = '1') then
-		count <= count + 1;
-		if count = TIMECONST then
-			count <= 0;
-			D <= not D;
-		end if;
-	end if;
-	CK_OUT <= D;
-end process;
-
+  process (CK_IN, D)
+  variable count : integer range 0 to 100000000;
+  begin
+    if rising_edge(CK_IN) then
+--    if (CK_IN'event and CK_IN = '1') then
+      count := count + 1;
+      if count = TIMECONST then
+        count := 0;
+        D <= not D;
+      end if;
+    end if;
+  end process;
+  CK_OUT <= D;
+  NCK_OUT <= not D;
 end Behavioral;
 
